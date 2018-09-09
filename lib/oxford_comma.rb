3cd3fb7f4309c5720d699a07bfa0536546700f17
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 18:53:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,11 @@
 def oxford_comma(array)
   case array.length
-  
-end
-  # return array.join if array.length == 1
-  array[-1] = "and #{array.last}" if array.length > 1
-  array.length > 2 ? array.join(", ") : array.join
-  # if array.length == 2
-  #   return array.join(" and ")
-  # else
-    # array[-1] = "and #{array.last}"
-    # array.join(", ")
-    # 
-  # end
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      array[-1] = "and #{array.last}"
+      array.join
+  end
 end

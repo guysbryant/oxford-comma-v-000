de9d05670831bb563b4cb88abf9f1543122f53e4
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 18:48:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   return array.join if array.length == 1
-  # array.length == 2 ? array.join(" and ") : array[-2] = ", and ".join(" ")
+  array[-1] = "and #{array.last}"
+  
   if array.length == 2
     return array.join(" and ")
   else
-    array[-1] = "and #{array.last}"
+    # array[-1] = "and #{array.last}"
     array.join(", ")
     
   end

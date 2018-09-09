73bcaa47fc85647e6c525c2f9d6ee74dec13843c
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 18:55:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,8 @@ def oxford_comma(array)
   #     array[-1] = "and #{array.last}"
   #     array.join(", ")
   # end
+  array[-1] = "and #{array.last}" if array.length > 1
+  array.length > 2 ? array.join(", ") : array.join(" ")
 end
 
 

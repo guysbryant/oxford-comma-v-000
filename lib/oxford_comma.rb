9a3b4ecfc416717406d11e60e181cf488733fea3
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 18:51:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,7 @@
 def oxford_comma(array)
+  case array.length
+  
+end
   # return array.join if array.length == 1
   array[-1] = "and #{array.last}" if array.length > 1
   array.length > 2 ? array.join(", ") : array.join

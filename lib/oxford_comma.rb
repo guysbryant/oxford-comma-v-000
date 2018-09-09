4973c4875aeca69379e2fb7d565f2cc6a6f231dd
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-09 18:57:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,25 +1,13 @@
 def oxford_comma(array)
-  # case array.length
-  #   when 1
-  #     array.join
-  #   when 2
-  #     array.join(" and ")
-  #   else
-  #     array[-1] = "and #{array.last}"
-  #     array.join(", ")
-  # end
+  case array.length
+    when > 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      array[-1] = "and #{array.last}"
+      array.join(", ")
+  end
   array[-1] = "and #{array.last}" if array.length > 1
   array.length > 2 ? array.join(", ") : array.join(" ")
 end
-
-
-# return array.join if array.length == 1
-  # array[-1] = "and #{array.last}" if array.length > 1
-  # array.length > 2 ? array.join(", ") : array.join
-  # if array.length == 2
-  #   return array.join(" and ")
-  # else
-    # array[-1] = "and #{array.last}"
-    # array.join(", ")
-    # 
-  # end
